29ff2c2d2f393e91607ccbe07372f6b47c976ff2
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-02-14 21:45:48 -0700

    Failing test case for #11

---
+++ src/test/cases/UnclosedMacroTestCase.hx
@@ -0,0 +1,25 @@
+package test.cases;
+
+import utest.Test;
+import utest.Assert;
+import kiss.Prelude;
+import kiss.List;
+import kiss.Stream;
+import haxe.ds.Option;
+import kiss.Kiss;
+#if js
+import js.lib.Promise;
+#end
+
+using StringTools;
+
+@:build(kiss.Kiss.buildExpectingError(kiss.EType.EUnmatchedBracket("(")))
+class UnclosedMacroTestCase extends Test {
+    function testSplitByAll() {
+        Assert.pass();
+    }
+
+    function testExpectedError() {
+        _testExpectedError();
+    }
+}
