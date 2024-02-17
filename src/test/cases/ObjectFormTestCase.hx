75b17384dcb863da02e12eaced399e81d92b4e65
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-02-17 08:55:27 -0700

    Failing test case for #7

---
+++ src/test/cases/ObjectFormTestCase.hx
@@ -0,0 +1,21 @@
+package test.cases;
+
+import utest.Test;
+import utest.Assert;
+import kiss.Prelude;
+import kiss.List;
+import haxe.ds.Option;
+import kiss.Kiss;
+#if js
+import js.lib.Promise;
+#end
+
+using StringTools;
+
+@:build(kiss.Kiss.build())
+class ObjectFormTestCase extends Test {
+    function testObjectFormWithAlias() {
+        _testObjectFormWithAlias();
+    }
+}
+ 
